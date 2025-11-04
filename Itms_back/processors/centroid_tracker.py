0917db4dfcb36be7656ee59ec018567cee03f47e
Anuubhavvvv <86676728+Anuubhavvvv@users.noreply.github.com>
2025-11-04 13:13:11 +0530

    Add files via upload

---
+++ Itms_back/processors/centroid_tracker.py
@@ -0,0 +1,68 @@
+from scipy.spatial import distance as dist
+from collections import OrderedDict
+import numpy as np
+
+class CentroidTracker:
+    def __init__(self, max_disappeared=40):
+        self.nextObjectID = 0
+        self.objects = OrderedDict()       # id -> centroid
+        self.disappeared = OrderedDict()   # id -> frame count
+        self.max_disappeared = max_disappeared
+
+    def register(self, centroid):
+        self.objects[self.nextObjectID] = centroid
+        self.disappeared[self.nextObjectID] = 0
+        self.nextObjectID += 1
+
+    def deregister(self, objectID):
+        del self.objects[objectID]
+        del self.disappeared[objectID]
+
+    def update(self, rects):
+        if len(rects) == 0:
+            for objectID in list(self.disappeared.keys()):
+                self.disappeared[objectID] += 1
+                if self.disappeared[objectID] > self.max_disappeared:
+                    self.deregister(objectID)
+            return self.objects
+
+        input_centroids = np.zeros((len(rects), 2), dtype="int")
+        for (i, (x1, y1, x2, y2)) in enumerate(rects):
+            cX = int((x1 + x2) / 2.0)
+            cY = int((y1 + y2) / 2.0)
+            input_centroids[i] = (cX, cY)
+
+        if len(self.objects) == 0:
+            for i in range(len(input_centroids)):
+                self.register(input_centroids[i])
+        else:
+            objectIDs = list(self.objects.keys())
+            objectCentroids = list(self.objects.values())
+
+            D = dist.cdist(np.array(objectCentroids), input_centroids)
+            rows = D.min(axis=1).argsort()
+            cols = D.argmin(axis=1)[rows]
+
+            usedRows, usedCols = set(), set()
+            for (row, col) in zip(rows, cols):
+                if row in usedRows or col in usedCols:
+                    continue
+                objectID = objectIDs[row]
+                self.objects[objectID] = input_centroids[col]
+                self.disappeared[objectID] = 0
+                usedRows.add(row)
+                usedCols.add(col)
+
+            unusedRows = set(range(0, D.shape[0])).difference(usedRows)
+            unusedCols = set(range(0, D.shape[1])).difference(usedCols)
+
+            for row in unusedRows:
+                objectID = objectIDs[row]
+                self.disappeared[objectID] += 1
+                if self.disappeared[objectID] > self.max_disappeared:
+                    self.deregister(objectID)
+
+            for col in unusedCols:
+                self.register(input_centroids[col])
+
+        return self.objects
